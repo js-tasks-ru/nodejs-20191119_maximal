4a026f00501d5274eb0bbd82a2152c234f960fe9
marutyunyan <marutyunyan@gpm.ru>
2019-11-19 13:59:14 +0300

    Let's Get Ready To Rumble! (c)Michael Buffer

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,5 +1,8 @@
 function sum(a, b) {
-  /* ваш код */
+  if (typeof(a) != 'number' || typeof(b) != 'number')
+    throw new TypeError('Arguments Must be a Numbers!');
+  
+  return a + b;
 }
 
 module.exports = sum;
