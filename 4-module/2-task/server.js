6c14970b02554a29918771a7616f12278ff99c09
marutyunyan <marutyunyan@gpm.ru>
2019-12-04 18:48:38 +0300

    Solution 4m2t var2

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -4,26 +4,23 @@ const path = require('path');
 const fs = require('fs');
 const { finished } = require('stream');
 const LimitSizeStream = require('./LimitSizeStream');
-//const LimitExceededError = require('./LimitExceededError');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
+  let file = '';
 
-  function SendError(err, errCode, file) {
-    //console.log('MAX: SendError ' + err + ' --- ' + file.path);
-
+  function SendError(err, errCode) {
     if (file) {
       file.once('close', () => {
         if (fs.existsSync(file.path)) 
           fs.unlinkSync(file.path);    
-          //console.log('MAX: SendError unlink');  
-          res.statusCode = errCode;
-          res.end(`Server Error: ${err.message}`); 
+
+        res.statusCode = errCode;
+        res.end(`Server Error: ${err.message}`); 
       })
 
       file.close();
-      //console.log('MAX: SendError close');
     }
   }
 
@@ -33,40 +30,37 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'POST':
-        //console.log('MAX: NEW POST ' + pathname);
     
-        if (/\//.test(pathname)) {
-          res.statusCode = 400;
-          res.end('File Not Found (SubDirectories are Not supported)');
-          break;        
-        }
+      if (/\//.test(pathname)) {
+        res.statusCode = 400;
+        res.end('File Not Found (SubDirectories are Not supported)');
+        break;        
+      }
 
-        if (fs.existsSync(filepath)) {
-          res.statusCode = 409;
-          res.end('File allready exists');
-          break;
-        }        
+      if (fs.existsSync(filepath)) {
+        res.statusCode = 409;
+        res.end('File allready exists');
+        break;
+      }        
 
-      const file = fs.createWriteStream(filepath);
+      file = fs.createWriteStream(filepath);
       file.on('error', SendError);
       
       const limitStream = new LimitSizeStream({limit: 1*1024*1024});
-        limitStream.on('error', (err) => {
-          SendError(err, 413, file);
+      limitStream.on('error', (err) => {
+        SendError(err, 413);
       });
       
       finished(req, (err) => {
         if (err) {
-          //console.log('MAX:finished: Stream error');
-          SendError(err, 500, file);
+          SendError(err, 500);
         } else {
-          //console.log('MAX:finished: Stream is done reading.');
           file.close;
+
           res.statusCode = 201;
           res.end('File Saved.');        
-         }
+        }
         req.resume();
-        //console.log('MAX:finished: Resumed.');
       });   
 
       req.pipe(limitStream).pipe(file);
